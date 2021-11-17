f7095b180664008951c6820fabb8da01b59adebf
DESKTOP-E7QL2EF\yangseehee <yangsee1951@naver.com>
2021-11-17 21:27:41 +0900

    로딩창과 내 사진 등록

--- test.js
+++ test.js
@@ -10,19 +10,19 @@ menu.onclick = () =>{
 
 window.onscroll = () =>{
 
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
 
-  if(window.scrollY > 60){
+   if(window.scrollY > 60){
     document.querySelector('#scroll-top').classList.add('active');
-  }else{
+   }else{
      document.querySelector('#scroll-top').classList.remove('active');
-}
+  }
 
 }
 
 function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out')
+  document.querySelector('.loader-container').classList.add('fade-out');
 }
 
 function fadeOut(){
